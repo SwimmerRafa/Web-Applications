022ffbab62f4d9362e31b7e61b7bce41bc0e7977
Felipe Osornio <felipe.osrn@gmail.com>
2020-05-28 20:16:57 -0500

    Bugs fixed

--- baraja/src/App.js
+++ baraja/src/App.js
@@ -1,89 +1,78 @@
-import React, {useState, useEffect, useReducer} from 'react';
+import React, {useEffect, useState} from 'react';
 //import logo from './logo.svg';
 import './App.css';
-import {cartas} from './cartas.json';
-import axios from "axios"
+import Axios from 'axios'
 import FormularioCarta from "./components/formularioCarta.jsx"
 
 function App() {
-  const cartas = []
-  
-  useEffect(() =>{
-    console.log("Effect componentDidMount!")
-  },[])
-  
-   useEffect(() =>{
-     if(state.jugador != ""){
-       console.log("Effect componentDidUpdate!")
-     }
-  },[])
-  
-  const cartasReducer = (state, action) =>{
-    switch(action.type){
-        case "PEDIR_CARTA":
-            return([
-                ...state, action.carta
-                ])
-        case "TIRAR_CARTA":
-            return state.filter((c)=> c!== action.carta)
-                
-    }
-  }
-  
-  const [cartasC, dispatch] = useReducer(cartasReducer, [])
-  const cartaControlador = {mazo:"", valor: "", numero: 0}
-  const [state, setState] = useState(cartaControlador)
-  
-  const tirarCarta =  (mazo, numero, valor) =>{
-        dispatch({
-            type: "TIRAR_CARTA",
-            mazo,
-            numero,
-            valor
+    const [cartas, setCartas] = useState([])
+
+    const tirarCarta = (id) => {
+        setCartas([cartas.filter((carta, idx) => idx !== id)])
+
+        Axios({
+            method: 'post',
+            url: 'http://3.23.131.19:8080/carta',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            data: {
+                titulo: id,
+                mensaje: 'Hello post'
+            }
         })
     }
-    
-  const pedirCarta = (e) =>{
-        e.preventDefault()
-        
-        dispatch({
-            type: "PEDIR_CARTA"
-        })
+
+    const agregarCarta = (carta) => {
+        setCartas([...cartas, carta])
     }
-    const cartasActuales = cartas.map((carta, i)=>{
-      return(
-        <div className= "col-md-4" key={i}>
-          <div className= "card mt-4">
-            <div className = "card-header">
-              <h3> Carta {i+1} </h3>
+
+    useEffect(() => {
+        Axios({
+            method: 'get',
+            url: 'http://3.23.131.19:8080/cartas'
+        }).then((res) => {
+            setCartas({...res.data})
+        })
+    })
+
+    const cartasActuales = cartas.map((carta, idx) => {
+        return (
+            <div className="col-md-4" key={idx}>
+                <div className="card mt-4">
+                    <div className="card-header">
+                        <h3> Carta {idx + 1}</h3>
+                    </div>
+                    <div className="card-body">
+                        <h3>Mazo: {carta.mazo}</h3>
+                        <h4>Numero: {carta.numero}</h4>
+                        <h5>Valor: {carta.valor}</h5>
+                    </div>
+                    <div className="card-footer">
+                        <button type="button" onClick={() => tirarCarta(idx)} className="btn btn-danger">Tirar Carta</button>
+                    </div>
+                </div>
             </div>
-            <div className= "card-body">
-              <h3>Mazo: {carta.mazo}</h3>
-              <h4>Número: {carta.numero}</h4>
-              <h5>Valor: {carta.valor}</h5>
-            </div>
-            <div className="card-footer">
-              <button type="button" onClick={tirarCarta(i)} className="btn btn-outline-danger">Tirar Carta</button>
-            </div>
-          </div>
-        </div>
-        );
-    });
+        )
+    })
+
+    console.log(cartasActuales)
+
     return (
-      <div>
-        <nav className = "navbar navbar-dark bg-dark">
-          <a className = "navbar-brand" href="/">
-            Número de Cartas <span className = "badge badge-pill badge-light">
-                                {cartas.length}
-                            </span>
-          </a>
-        </nav>
-        <FormularioCarta onAgregarCarta={pedirCarta}/>
-        <div> 
-          {cartasActuales}
+        <div>
+            <nav className="navbar navbar-dark bg-dark">
+                <a className="navbar-brand" href="/">
+                    Número de Cartas <span className="badge badge-pill badge-light">
+                        {cartas.length}
+                    </span>
+                </a>
+            </nav>
+            <FormularioCarta onAgregarCarta={agregarCarta}/>
+            <div>
+                {cartasActuales}
+            </div>
         </div>
-      </div>
-    );
+    )
 }
 
 export default App;
--- baraja/src/components/formularioCarta.jsx
+++ baraja/src/components/formularioCarta.jsx
@@ -1,49 +1,49 @@
 import React, {useState} from 'react';
 
-const FormularioCarta = () =>{
-    
-    const cartasControlador = {mazo:"Corazones", valor: "", numero: ""}
+const FormularioCarta = (props) => {
+
+    const cartasControlador = {mazo: "Corazones", valor: "", numero: ""}
     const [state, setState] = useState(cartasControlador)
-    
+
     const handleInput = (evt) => {
-            evt.preventDefault()
-            const {name, value} = evt.target
-            //setState(prevState => ({...prevState, [name]: value}))
-            setState= ({[name]:value})
-            // [name] => [evt.target.name] => ['valor']: value | state[name] == state.name 
-        }
-    
-    const handleSubmit=(e) =>{
-      e.preventDefault()
-      this.props.onAgregarCarta(state)
-      
+        evt.preventDefault()
+        const {name, value} = evt.target
+        setState(prevState => ({...prevState, [name]: value}))
+        // [name] => [evt.target.name] => ['valor']: value | state[name] == state.name
     }
-        
-    return(
+
+    const handleSubmit = (evt) => {
+        evt.preventDefault()
+        props.onAgregarCarta(state)
+    }
+
+    return (
         <div>
-            <div className= "card">
-                <form className= "card-body" onSubmit={handleSubmit}>
+            <div className="card">
+                <form className="card-body" onSubmit={handleSubmit}>
                     <div className="form-group">
                         <select value={state.mazo} onChange={(evt) => handleInput(evt)} name="mazo" className="form-control" placeholder="Mazo">
-                           <option>Corazones</option>
-                           <option>Treboles</option>
-                           <option>Espada</option>
-                           <option>Diamantes</option>
+                            <option>Corazones</option>
+                            <option>Treboles</option>
+                            <option>Espada</option>
+                            <option>Diamantes</option>
                         </select>
                     </div>
                     <div className="form-group">
-                         <input value={state.numero} onChange={(evt) => handleInput(evt)} type= "text" name= "numero" className="form-control" placeholder="Número"/>
+                        <input value={state.numero} onChange={(evt) => handleInput(evt)} type="text" name="numero" className="form-control"
+                               placeholder="Número"/>
                     </div>
                     <div className="form-group">
-                         <input value={state.valor} onChange={(evt) => handleInput(evt)} type= "number" name= "valor" className="form-control" placeholder="Valor"/>
+                        <input value={state.valor} onChange={(evt) => handleInput(evt)} type="number" name="valor" className="form-control"
+                               placeholder="Valor"/>
                     </div>
-                    <button type="submit" className="btn btn-primary"> Agregar Carta </button>
+                    <button type="submit" className="btn btn-primary"> Agregar Carta</button>
                 </form>
             </div>
         </div>
-        )    
+    )
 
-} 
+}
 
 
 export default FormularioCarta
